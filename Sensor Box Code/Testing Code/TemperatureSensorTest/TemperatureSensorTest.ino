9fbc41c093f67cc1890b395f788daf221d444a13
Ray-Song888 <ray.ruifeng.song@gmail.com>
2025-06-09 11:38:34 -0400

    T&RH sensor test code changes
    Added descriptions to the reading output

--- Sensor Box Code/Testing Code/TemperatureSensorTest/TemperatureSensorTest.ino
+++ Sensor Box Code/Testing Code/TemperatureSensorTest/TemperatureSensorTest.ino
@@ -1,34 +1,24 @@
-
- 
 #include <Wire.h> // For I2C communication
-
 #include "AM2315C.h" // For T&RH sensor
- 
+
 AM2315C T_RH; // T&RH sensor
- 
+
 void setup() {
- 
+
   Serial.begin(9600); // LTE and Arduino are using Serial port for communication
- 
+
   // Initialize Sensors
-
   Wire.begin();
-
   T_RH.begin();
-
   delay(5000); // For T&RH sensor to setup
-
-}//end setup()
- 
-void loop(){
-
-  T_RH.read();
-
-  Serial.print(T_RH.getTemperature());
-
-  Serial.print(T_RH.getHumidity());
   
-  delay(2000);
+}//end setup()
 
+void loop(){
+  T_RH.read();
+  Serial.print("Temperature: ");
+  Serial.print(T_RH.getTemperature());
+  Serial.print(", Relative Humidity: ");
+  Serial.println(T_RH.getHumidity());
+  delay(2000);
 }
- 
